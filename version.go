00159dbd1f787e86d9f072a6dbb3cffceb1cdf0a
harwck <22517004+harwck@users.noreply.github.com>
2025-08-25 18:11:24 +0900

    fix: Remove

--- version.go
+++ version.go
@@ -1,12 +1,3 @@
 package main
 
-import "os"
-
-func GetEnvWithFallback(key, fallback string) string {
-	if value := os.Getenv(key); value != "" {
-		return value
-	}
-	return fallback
-}
-
-var Version = GetEnvWithFallback("GOSU_VERSION", "PRE_RELEASE")
+const Version = "PRE_RELEASE"
